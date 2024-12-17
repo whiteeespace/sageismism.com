3e678b65691ae790f87da93fa4aa337f46864578
Yu Tong Liu <yutongliuytl@gmail.com>
2024-12-16 23:37:02 -0500

    Adds not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
     <div className={styles["error-container"]}>
       <h1>404 Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-      <Link href="/">
+      <Link href="/" className={styles["button-container"]}>
         <Button variant="primary">Return Home</Button>
       </Link>
     </div>
