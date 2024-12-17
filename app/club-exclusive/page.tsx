f7eb9e257e4a951af50fee3e4f3d4b9ffaee81cc
Yu Tong Liu <yutongliuytl@gmail.com>
2024-12-16 23:31:03 -0500

    Adds exclusive

---
+++ app/club-exclusive/page.tsx
@@ -0,0 +1,52 @@
+import classNames from "classnames";
+import { Metadata, ResolvingMetadata } from "next";
+import Link from "next/link";
+
+import Item from "@/components/shared/Item";
+import { ProductLabel } from "@utils/types/productLabel";
+
+import { getCollection } from "./action";
+import styles from "./styles.module.scss";
+
+export async function generateMetadata(_, parent: ResolvingMetadata): Promise<Metadata> {
+  const parentFields = await parent;
+
+  return {
+    title: "Sageism · shop",
+    description: "shop sageism jawns namsayin. shipping worldwide.",
+    metadataBase: parentFields.metadataBase,
+    ...parentFields.robots,
+  };
+}
+
+const ShopPage = async () => {
+  const { products } = await getCollection();
+
+  return (
+    <section className={classNames(styles["container"])}>
+      <div className={classNames(styles["products"])}>
+        {products?.map((product) => {
+          const labelInfo = product.label?.value
+            ? (JSON.parse(product.label.value) as ProductLabel)
+            : undefined;
+          return (
+            <Link href={`/product/${product.handle}`} key={product.handle}>
+              <Item
+                src={
+                  product.image?.reference?.__typename === "MediaImage" && product.image?.reference.image?.url
+                }
+                name={product.title}
+                productLabel={labelInfo}
+                price={product.priceRange?.minVariantPrice}
+                availableForSale={product.availableForSale}
+                productImage={product.featuredImage?.url}
+              />
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ShopPage;
